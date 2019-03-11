f9f98bd41cbe555f30b4b143836ecaa4823ee9c5
Eric Schultz <eschultz591@gmail.com>
2019-03-10 20:18:08 -0500

    First commit
    Includes assets from old project

---
+++ Standoff/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public GameObject Attack;
+
+    #region Movement Variables
+    [SerializeField] private float m_HorizontalSpeed;
+    [SerializeField] private float m_VerticalSpeed;
+
+    //limits speed of diagonal movement
+    private float moveLimiter = .7f;
+    private Vector3 MoveVector;
+    private Vector3 m_Velocity = Vector3.zero;
+    #endregion
+
+    [SerializeField]
+    public int health = 4;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    public void Move(float VSpeed, float HSpeed)
+    {
+
+        rb.velocity = new Vector3(HSpeed * moveLimiter, VSpeed * moveLimiter, 0);
+
+
+    }
+    
+    public void Click(Vector3 mouseLocation)
+    {
+       
+        Vector3 pos = mouseLocation;
+        pos.z = 18;
+        pos = Camera.main.ScreenToWorldPoint(pos);
+        Instantiate(Attack, pos, Quaternion.identity);
+
+
+
+
+        RaycastHit hit = new RaycastHit();
+
+
+        Debug.DrawRay(transform.position, pos);
+        Debug.Log("You clicked: " + (pos));
+        
+    }
+     
+    public void TakeDamage(int damage)
+    {
+        Debug.Log("oof");
+        health -= damage;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
---
+++ Standoff/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal") * runSpeed;
+        verticalMove = Input.GetAxis("Vertical") * runSpeed;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 mouseLocation = Input.mousePosition;
+            controller.Click(mouseLocation);
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(verticalMove, horizontalMove);
+
+
+
+    }
+}
