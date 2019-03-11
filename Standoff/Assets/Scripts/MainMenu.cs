deafa54e11bb35e472ab29363a187f31d68a7d41
unknown <mschultz591@gmail.com>
2019-03-11 14:52:52 -0500

    server test

--- Standoff/Assets/Scripts/MainMenu.cs
+++ Standoff/Assets/Scripts/MainMenu.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +10,20 @@ public class MainMenu : MonoBehaviour
 {
     public void CreateGame ()
     {
+         string localIP;
+
+        using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+        {
+             socket.Connect("8.8.8.8", 65530);
+             IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+             localIP = endPoint.Address.ToString();
+
+             
+        }
+
         SceneManager.LoadScene(1);
+        Debug.Log("Ip:"+ localIP);
+
+        
     }
 }
