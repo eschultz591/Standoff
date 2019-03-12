2dc5485b07b2cda3582a31a181a674c65fdfaa7a
unknown <mschultz591@gmail.com>
2019-03-12 04:03:56 -0500

    added to docs need to add game/server manager

--- Standoff/Assets/Scripts/MainMenu.cs
+++ Standoff/Assets/Scripts/MainMenu.cs
@@ -3,27 +3,59 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public int port = 7777;
     public void CreateGame ()
     {
-         string localIP;
-
-        using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
-        {
-             socket.Connect("8.8.8.8", 65530);
-             IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
-             localIP = endPoint.Address.ToString();
-
-             
-        }
-
         SceneManager.LoadScene(1);
-        Debug.Log("Ip:"+ localIP);
+         
 
         
+
+        byte[] bytes= new byte[256];
+        int recv;
+
+
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Udp);
+        socket.Connect("8.8.8.8", port);
+        IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+        int outport = ((IPEndPoint)endPoint).Port;
+       // int data = socket.Receive(bytes,0,socket.Available,SocketFlags.None);
+
+
+        string ip = socket.LocalEndPoint.AddressFamily.ToString();
+
+
+
+        Debug.Log("Ip:"+ ip);
+        Debug.Log("waiting for client on port: " + outport );
+        
+        
+
+        
+    }
+
+
+
+    public void JoinServer()
+    {
+        byte[] msg = Encoding.UTF8.GetBytes("This is a test");
+
+
+        Socket socket = new Socket(AddressFamily.InterNetwork,SocketType.Dgram,ProtocolType.Udp);
+
+
+        int bytecount = socket.Send(msg,0,msg.Length,SocketFlags.None); 
+        Debug.Log("Sent bytes" + bytecount);
+
+
+        socket.Connect("10.0.0.23",59843);
+        Debug.Log("connection made");
+
     }
 }
