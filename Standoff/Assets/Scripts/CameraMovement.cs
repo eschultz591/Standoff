a506372e020c97c3ed6d70998a00e37e2bd7ff23
Eric Schultz <eschultz591@gmail.com>
2019-03-13 00:28:34 -0500

    GM is creating scene just having a problem with the camera

--- Standoff/Assets/Scripts/CameraMovement.cs
+++ Standoff/Assets/Scripts/CameraMovement.cs
@@ -4,22 +4,39 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+  
     public GameObject player;       
 
-
+    //private Vector2 offset;
     private Vector3 offset;         
 
 
     void Start()
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        ///delete the bottom comment once you set up the code for camera lag
+       // offset = transform.position - player.transform.position;
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+       //Vector2 pV2 = Vector3Extension.AsVector2(player.transform.position);
         transform.position = player.transform.position + offset;
     }
+
+
+
+  
 }
+/*
+public static class Vector3Extension
+{
+    public static Vector2 AsVector2(this Vector3 _v)
+    {
+        return new Vector2(_v.x, _v.y);
+    }
+}
+
+    */
