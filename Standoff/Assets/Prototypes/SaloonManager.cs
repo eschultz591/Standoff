062bfe81421b90f60e7a87704cdfe7ff98a79d03
Eric Schultz <eschultz591@gmail.com>
2019-03-13 22:58:40 -0500

    Decent Progress on saloon manager

---
+++ Standoff/Assets/Prototypes/SaloonManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaloonManager : MonoBehaviour
+{
+    [SerializeField] private List<Stool> Stools = new List<Stool>();
+   
+
+
+    void Start()
+    {
+        int i = 0;
+        foreach (Transform child in (this.transform.parent).transform)
+        {
+            if (child.tag == "Stool")
+            {
+                Stools.Add(new Stool(i, false, child.gameObject.transform.position));
+                i++;
+            }
+          
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Standoff/Assets/Prototypes/Stool.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stool
+{
+    public int stoolNum;
+    public bool isTaken;
+    public Vector3 stoolLocation;
+    public Stool(int seatNum, bool taken, Vector3 spot)
+    {
+        stoolNum = seatNum;
+        isTaken = taken;
+        stoolLocation = spot;
+    }
+
+}
